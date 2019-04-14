01fd3d8d389989f3c634e70022073c154befae8e
Bhanuka Gamage <bhanukamgamage@gmail.com>
2019-04-15 05:25:50 +0800

    Added Json Conversions

--- ai_module/inference/preprocessing.py
+++ ai_module/inference/preprocessing.py
@@ -3,33 +3,72 @@ import datetime
 import sys
 import csv
 
-csv.field_size_limit(sys.maxsize)
+import csv
+import json
 
-dataset = [];
 
-readcsvfile = open('csv/test_large.csv')
-writecsv = open('csv/network.csv','w')
+def json2csv(json,file_name,new_file=False):
+    load_json = json.loads(json)
 
-readCSV = csv.reader(readcsvfile, delimiter=',')
-writeCSV = csv.writer(writecsv)
+    if new_file:
+        f = csv.writer(open(file_name, "wb+"))
+        f.writerow(["datetime","total","total_tcp","total_http","total_udp","size","size_tcp","size_http","size_udp"])
+    else:
+        f = csv.writer(open(file_name, "a"))
 
-header = readCSV.next()
-writeCSV.writerow(header[0:9])
-writeCSV.writerow(["datetime","float","float","float","float","float","float","float","float"])
-writeCSV.writerow(["T","","","","","","","",""])
+    for line in load_json:
+        f.writerow(
+            line["datetime"],
+            line["total"],
+            line["total_tcp"],
+            line["total_http"],
+            line["total_udp"],
+            line["size"],
+            line["size_tcp"],
+            line["size_http"],
+            line["size_udp"]
+            )
 
-for row in readCSV:
+def csv2json():
+    csvfile = open('file.csv', 'r')
+    jsonfile = open('file.json', 'w')
 
-    for i in range(0,9):
-        row[i] = float(row[i])
+    fieldnames = ("datetime","total","total_tcp","total_http","total_udp","size","size_tcp","size_http","size_udp")
+    reader = csv.DictReader( csvfile, fieldnames)
+    for row in reader:
+        json.dump(row, jsonfile)
+        jsonfile.write('\n')
 
-    dt = datetime.datetime.fromtimestamp(row[0])
-    dt = dt.strftime('%Y-%m-%d %H:%M:%S')
 
-    updated_row = row
-    updated_row[0] = dt
-    final_output = updated_row[0:9]
-    writeCSV.writerow(final_output)
+def removeTimeStamp():
+    csv.field_size_limit(sys.maxsize)
+
+    dataset = [];
+
+    readcsvfile = open('csv/test_large.csv')
+    writecsv = open('csv/network.csv','w')
+
+    readCSV = csv.reader(readcsvfile, delimiter=',')
+    writeCSV = csv.writer(writecsv)
+
+    header = readCSV.next()
+    writeCSV.writerow(header[0:9])
+    writeCSV.writerow(["datetime","float","float","float","float","float","float","float","float"])
+    writeCSV.writerow(["T","","","","","","","",""])
+
+    for row in readCSV:
+
+        for i in range(0,9):
+            row[i] = float(row[i])
+
+        dt = datetime.datetime.fromtimestamp(row[0])
+        dt = dt.strftime('%Y-%m-%d %H:%M:%S')
+
+        updated_row = row
+        updated_row[0] = dt
+        final_output = updated_row[0:9]
+        writeCSV.writerow(final_output)
 
 def filter(astr):
     return astr.replace("[","").replace("]","").replace(" ","").replace("'","").split(",")
+
